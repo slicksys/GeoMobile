f66f958aa6b485f0f3e6126dc9d62046dbab3ede
EllisHuang <ellishuang17@gmail.com>
2014-04-02 01:29:13 +0800

    Add Location Update

--- src/com/example/geomobile/MainActivity.java
+++ src/com/example/geomobile/MainActivity.java
@@ -4,58 +4,65 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.webkit.JavascriptInterface;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity implements LocationListener{
+import com.example.geomobile.GeoLocation.LocationResult;
+
+public class MainActivity extends Activity{
 	
-	private static final String MAP_URL = "file:///android_asset/map.html";
-	LocationManager mLocationManager = null;
-	Location mLastLocation = null;
+	private static final String MAP_URL = "file:///android_asset/showmap.html";
+	private double nowLat, nowLng;
 	WebView mWebView = null;
+	GeoLocation geoLocation = new GeoLocation();
+
+	LocationResult locationResult = new LocationResult() {
+		@Override
+		public void gotLocation(Location location) {
+			if (location != null) {
+				nowLat = location.getLatitude();
+				nowLng = location.getLongitude();
+				mWebView.loadUrl("javascript:moveTo(" + nowLat + "," + nowLng
+						+ ")");
+				mWebView.setVisibility(View.VISIBLE);
+			}
+		}
+	};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		mLocationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-		mLastLocation = mLocationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+		geoLocation.getLocation(this, locationResult);
 		
 		mWebView = (WebView) findViewById(R.id.webview);
-		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.setWebViewClient(new WebViewClient());
+		mWebView.getSettings().setJavaScriptEnabled(true);
 		mWebView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
 		mWebView.getSettings().setSupportZoom(true);
 		mWebView.getSettings().setBuiltInZoomControls(true);
 		mWebView.loadUrl(MAP_URL);
 
-		mWebView.addJavascriptInterface(new JavaScriptInterface(), "android");
+		mWebView.addJavascriptInterface(new FromJavaScript(this), "Android");
 
 		setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-	}
 
-	@Override
-	protected void onPause() {
-		// TODO Auto-generated method stub
-		super.onPause();
-		
-		mLocationManager.removeUpdates(this);
-	}
-
-	@Override
-	protected void onResume() {
-		// TODO Auto-generated method stub
-		super.onResume();
-		
-		mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 60000, 1, this);
+		Button btnUpdate = (Button)findViewById(R.id.btn_update);
+		btnUpdate.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				geoLocation.getLocation(MainActivity.this, locationResult);
+			}
+		});
 	}
 
 	@Override
@@ -65,57 +72,22 @@ public class MainActivity extends Activity implements LocationListener{
 		return true;
 	}
 
-	private class JavaScriptInterface {
-		
-		@JavascriptInterface
-        public double getLatitude() {
-			if (mLastLocation == null) {
-				return Double.parseDouble("23.937591");
-			}
-			
-			double latitude = mLastLocation.getLatitude();
-			Log.e("GeoMobile", "Latitude:"+latitude);
-            return Double.parseDouble(Double.toString(latitude));
-        }
+	private class FromJavaScript {
+		private Context c;
+
+		FromJavaScript(Context c) {
+			this.c = c;
+		}
 
 		@JavascriptInterface
-        public double getLongitude() {
-			if (mLastLocation == null) {
-				return Double.parseDouble("120.700807");
-			}
-			
-			double longtude = mLastLocation.getLongitude();
-			Log.e("GeoMobile", "Latitude:"+longtude);
-            return Double.parseDouble(Double.toString(longtude));
-        }
-
-    }
-
-	@Override
-	public void onLocationChanged(Location arg0) {
-		// TODO Auto-generated method stub
-		mLastLocation = arg0;
-		if (mLastLocation != null) {
-			Log.e("GeoMobile", "Latitude:"+mLastLocation.getLatitude()+", Latitude:"+ mLastLocation.getLongitude() );
-			mWebView.loadUrl(MAP_URL);
+		public double getLatLng(String data) {
+			//Message msg = new Message();
+			//Bundle db = new Bundle();
+			//db.putString("data", data);
+			//msg.setData(db);
+			Toast.makeText(c, data, Toast.LENGTH_LONG).show();
+			return 0;
 		}
 	}
-
-	@Override
-	public void onProviderDisabled(String arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onProviderEnabled(String arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
